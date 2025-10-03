93696ff5f5b67f28f6a7405fd5afe0186c8200b8
mixail <v1tus5555@yandex.by>
2025-10-03 20:42:45 +0300

    Вынесим всю служебную информацию о компании в appsettings.json и создадим класс для работы с контактной информацией компании Appconfig.cs

---
+++ CrafterCompany/Infrastructure/Appconfig.cs
@@ -0,0 +1,29 @@
+﻿using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace CrafterCompany.Infrastructure
+{
+    public class Appconfig
+    {
+        public TinyMCE TinyMCE { get; set; } = new TinyMCE();
+        public Company Company { get; set; } = new Company();
+        public Database Database { get; set; } = new Database();
+    }
+
+    public class Database
+    {
+        public string? ConnectionString { get; set; }
+
+    }
+    public class TinyMCE
+    {
+        public string? APIKey { get; set; }
+
+    }
+    public class Company
+    {
+        public string? CompanyName { get; set; }
+        public string? CompanyPhone { get; set; }
+        public string? CompanyPhoneShort { get; set; }
+        public string? CompanyEmail { get; set; }
+    }
+}

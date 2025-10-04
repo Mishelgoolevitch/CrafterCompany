e21ec4609dc9207d6d8b5ea848a853a40bb730c7
mixail <v1tus5555@yandex.by>
2025-10-04 21:03:28 +0300

    Заполняем контентом _FooterPartial и _HeaderPartial и _SidebarPartial и создаем контролер HomeController и страничку Index.cshtml

---
+++ CrafterCompany/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CrafterCompany.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

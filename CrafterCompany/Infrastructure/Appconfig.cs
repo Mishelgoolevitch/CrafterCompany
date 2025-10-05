438be6fd6f4848e2ad1dad4807767e25555da579
mixail <v1tus5555@yandex.by>
2025-10-05 18:43:48 +0300

    Программ файле подключаем контроллер, Подключаем в конфигурацию файл appsettings Оборачиваем секцию Project в объектную форму Подключаем статичные файлы Подключаем систему маршрутизации Регистрируем нужные нам маршруты

--- CrafterCompany/Infrastructure/Appconfig.cs
+++ CrafterCompany/Infrastructure/Appconfig.cs
@@ -2,7 +2,7 @@
 
 namespace CrafterCompany.Infrastructure
 {
-    public class Appconfig
+    public class AppConfig
     {
         public TinyMCE TinyMCE { get; set; } = new TinyMCE();
         public Company Company { get; set; } = new Company();
--- CrafterCompany/Program.cs
+++ CrafterCompany/Program.cs
@@ -1,15 +1,40 @@
+using CrafterCompany.Infrastructure;
+
 namespace CrafterCompany
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+         WebApplicationBuilder    builder = WebApplication.CreateBuilder(args);
+           
+            //Подключаем в конфигурацию файл appsettings
+            IConfigurationBuilder configBuild = new ConfigurationBuilder()
+                .SetBasePath(builder.Environment.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
 
-            app.MapGet("/", () => "Hello World!");
+            //Оборачиваем секцию Project в объектную форму
+            IConfiguration configuration = configBuild.Build();
+            AppConfig config = configuration.GetSection("Project").Get<AppConfig>()!;
 
-            app.Run();
+            //Подключаем функционал контролеров
+            builder.Services.AddControllersWithViews();
+
+            //Собираем конфигурацию
+            WebApplication app = builder.Build();
+
+            //Подключаем использование статичных файлов (js, css)
+            app.UseStaticFiles();
+
+            //Подключаем систему маршрутизации
+            app.UseRouting();
+
+
+            //Регистрируем нужные нам маршруты
+            app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+            await app.RunAsync();
         }
     }
 }

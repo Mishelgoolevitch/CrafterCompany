91aa1aac54c311cd3777077f6a56c42267acb16e
mixail <v1tus5555@yandex.by>
2025-10-08 19:11:13 +0300

    Creating interface IEquipmentsRepository in folder Domain.Repositories.Abstract-incapsulatung all the logic in the interfase IEquipmentsRepository and creating class EFEquipmentsRepository whith will implement IEquipmentsRepository

---
+++ CrafterCompany/Domain/Repositories/Abstract/IEquipmentsRepository.cs
@@ -0,0 +1,12 @@
+﻿using CrafterCompany.Domain.Entities;
+
+namespace CrafterCompany.Domain.Repositories.Abstract
+{
+    public interface IEquipmentsRepository
+    {
+        Task<IEnumerable<Equipment>> GetServicesAsync();
+        Task<Equipment?> GetServiceByIdAsync(int id);
+        Task SaveServiceAsync(Equipment entity);
+        Task DeleteServiceAsync(int id);
+    }
+}
---
+++ CrafterCompany/Domain/Repositories/EntityFramework/EFEquipmentsRepository.cs
@@ -0,0 +1,39 @@
+﻿using CrafterCompany.Domain.Entities;
+using CrafterCompany.Domain.Repositories.Abstract;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrafterCompany.Domain.Repositories.EntityFramework
+{
+    public class EFEquipmentsRepository:IEquipmentsRepository
+    {
+        private readonly AppDbContext _context;
+
+        public EFEquipmentsRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        //Services
+        public async Task<IEnumerable<Equipment>> GetServicesAsync()
+        {
+            return await _context.Equipments.Include(x => x.Service).ToListAsync();
+
+        }
+
+        public async Task<Equipment?> GetServiceByIdAsync(int id)
+        {
+            return await _context.Equipments.Include(x => x.Service).FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task SaveServiceAsync(Equipment entity)
+        {
+            _context.Entry(entity).State = entity.Id == default ? EntityState.Added : EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteServiceAsync(int id)
+        {
+            _context.Entry(new Equipment() { Id = id }).State = EntityState.Deleted;
+            await _context.SaveChangesAsync();
+        }
+    }
+}

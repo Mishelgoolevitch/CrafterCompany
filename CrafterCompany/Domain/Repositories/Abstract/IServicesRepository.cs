edda3099f81b2492af19a07f55994c67722b720e
mixail <v1tus5555@yandex.by>
2025-10-08 18:38:23 +0300

    Creating interface IServicesRepository in folder Domain.Repositories.Abstract-incapsulatung all the logic in the interfase IServicesRepository and creating class EFServicesRepository whith will implement IServicesRepository

---
+++ CrafterCompany/Domain/Repositories/Abstract/IServicesRepository.cs
@@ -0,0 +1,12 @@
+﻿using CrafterCompany.Domain.Entities;
+
+namespace CrafterCompany.Domain.Repositories.Abstract
+{
+    public interface IServicesRepository
+    {
+        Task<IEnumerable<Service>> GetServicesAsync();
+        Task<Service?> GetServiceByIdAsync(int id);
+        Task SaveServiceAsync(Service entity);
+        Task DeleteServiceAsync(int id);
+    }
+}
---
+++ CrafterCompany/Domain/Repositories/EntityFramework/EFServicesRepository.cs
@@ -0,0 +1,39 @@
+﻿using CrafterCompany.Domain.Entities;
+using CrafterCompany.Domain.Repositories.Abstract;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrafterCompany.Domain.Repositories.EntityFramework
+{
+    public class EFServicesRepository : IServicesRepository
+    {
+        private readonly AppDbContext _context;
+
+        public EFServicesRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        //Services
+        public async Task<IEnumerable<Service>> GetServicesAsync()
+        {
+            return await _context.Services.Include(x => x.ServiceCategory).ToListAsync();
+
+        }
+
+        public async Task<Service?> GetServiceByIdAsync(int id)
+        {
+            return await _context.Services.Include(x => x.ServiceCategory).FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task SaveServiceAsync(Service entity)
+        {
+            _context.Entry(entity).State = entity.Id == default ? EntityState.Added : EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteServiceAsync(int id)
+        {
+            _context.Entry(new Service() { Id = id }).State = EntityState.Deleted;
+            await _context.SaveChangesAsync();
+        }
+    }
+}

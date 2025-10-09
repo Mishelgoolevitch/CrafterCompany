833cc0d53937038b3f7d63fd2b3dfd8da3dddc32
mixail <v1tus5555@yandex.by>
2025-10-09 08:02:15 +0300

    Adding class DataManager in folder Domain - creating a wrapper class over all repositories, just concentrate in one place.

---
+++ CrafterCompany/Domain/DataManager.cs
@@ -0,0 +1,18 @@
+﻿using CrafterCompany.Domain.Repositories.Abstract;
+
+namespace CrafterCompany.Domain
+{
+    public class DataManager
+    {
+        public IServiceCategoriesRepository ServiceCategories { get; set; }
+        public IServicesRepository Services { get; set; }
+        public IEquipmentsRepository Equipments { get; set; }
+
+        public DataManager(IServiceCategoriesRepository serviceCategoriesRepository, IServicesRepository servicesRepository, IEquipmentsRepository equipments)
+        {
+            ServiceCategories = serviceCategoriesRepository;
+            Services = servicesRepository;
+            Equipments = equipments;
+        }
+    }
+}

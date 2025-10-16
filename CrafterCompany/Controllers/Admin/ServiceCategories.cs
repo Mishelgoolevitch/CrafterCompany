f58d2e92cbfe541ba1d306d66f1602ee11036764
mixail <v1tus5555@yandex.by>
2025-10-16 19:10:32 +0300

    Adding functions ServiceCategoriesEdit, ServiceCategoriesDelete in partial class AdminController

--- CrafterCompany/Controllers/Admin/ServiceCategories.cs
+++ CrafterCompany/Controllers/Admin/ServiceCategories.cs
@@ -1,6 +1,34 @@
-﻿namespace CrafterCompany.Controllers.Admin
+﻿using CrafterCompany.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrafterCompany.Controllers.Admin
 {
     public partial class AdminController
     {
+        public async Task<IActionResult> ServiceCategoriesEdit(int id)
+        {
+            ServiceCategory? entity = id == default
+                ? new ServiceCategory()
+                : await _dataManager.ServiceCategories.GetServiceCategoryByIdAsync(id);
+            return View(entity);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ServiceCategoriesEdit(ServiceCategory entity)
+        {
+            if (!ModelState.IsValid)
+                return View(entity);
+            await _dataManager.ServiceCategories.SaveServiceCategoryAsync(entity);
+            _logger.LogInformation($"Добавлена/обновлена категория услуги с ID:{entity.Id}");
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ServiceCategoriesDelete(int id)
+        {
+            await _dataManager.ServiceCategories.DeleteServiceCategoryAsync(id);
+            _logger.LogInformation($"Удалена категория услуги с ID:{id}");
+            return RedirectToAction("Index");
+        }
     }
 }
